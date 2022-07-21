effc7a9f59b3d93b8e8a8df47e23698f7cfa0263
gabao55 <gabriel.s.rosin@gmail.com>
2022-07-21 09:34:01 -0300

    Implements results on the bottom of the page

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -6,6 +6,19 @@ export default function Result({ results }) {
     return (
         <footer className="result">
             <p>{questionsWithResult}/4 CONCLUÍDOS</p>
+            <div>
+                {results.map((result) => {
+                    if (result === "no") {
+                        return <ion-icon name="close-circle" style={{ color: "#FF3030"}}></ion-icon>
+                    }
+                    if (result === "maybe") {
+                        return <ion-icon name="help-circle" style={{ color: "#FF922E"}}></ion-icon>
+                    }
+                    if (result === "yes") {
+                        return <ion-icon name="checkmark-circle" style={{ color: "#2FBE34"}}></ion-icon>
+                    }
+                })}
+            </div>
         </footer>
     )
 }
